8c881f13d923d3dc810868d07122406c2e742fb4
vtjesus <chernikow.bogdan2006@gmail.com>
2024-09-13 02:32:46 +0300

    first commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+import { Separator } from "@/components/ui/separator"
+
+export const Footer = () => {
+    return (
+        <footer className="mt-5 ">
+            <Separator/>
+            <div className="my-5 text-center text-sm opacity-50">
+                Create by @ <a href="https://www.linkedin.com/in/vtjesus-vtjesus-a81bb2326/" target="blank_">VTJesus </a>
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/products/empty.tsx
@@ -0,0 +1,8 @@
+export const ProductEmpty = () => {
+    return(
+        <div className="w-full my-10 text-center">
+            <p className="text-3xl font-bold">Ops!</p>
+            <p className="mt-5">Unfortunately, we have no products in this category.</p>
+        </div>
+    )
+}
